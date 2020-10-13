dffef99f3dc3bd593753cc17cc5ed98c8c92d804
Abhishek Raj <abhi@raj.me>
2020-10-13 19:03:05 +0530

    Setup express

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,36 @@
-const server = () => {
-  console.log('Hello from server!');
-}
+import express from 'express';
+import fs from 'fs';
 
-server();
+import { checkProd } from 'src/utils/env.utils';
+
+const app = express();
+const PORT = 3000;
+
+const isProd = checkProd();
+
+// hide powered by express
+app.disable('x-powered-by');
+
+// disallow caching of JavaScript/CSS bundles in dev mode
+app.use((req, res, next) => {
+  if (!isProd && req.url.match(/\.(js|css)$/i)) {
+    res.setHeader('Cache-Control', 'no-store');
+  }
+  return next();
+});
+
+// serve static assets
+app.use(express.static('build/public'));
+
+app.get('/*', (req, res) => {
+  const filePath = process.cwd() + `/build/public/index.html`;
+  if (fs.existsSync(filePath)) {
+    res.sendFile(filePath);
+  } else {
+    res.send('Please refresh!');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`\nApp running at port ${PORT} 😎\n`);
+});
