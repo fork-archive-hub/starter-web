935ed4c7f441c9d6713a316f7a03bb8593c8ab65
Abhishek Raj <abhi@raj.me>
2020-10-17 00:40:21 +0530

    Add react support for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,14 @@ module.exports = {
     ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
     sourceType: "module", // Allows for the use of imports
     project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true // Allows for the parsing of JSX
+    }
+  },
+  settings: {
+    react: {
+      version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
+    }
   },
   env: {
     "browser": true,
@@ -12,9 +20,11 @@ module.exports = {
   },
   plugins: [
     "@typescript-eslint",
+    "eslint-plugin-react",
   ],
   extends: [
     "eslint:recommended",
+    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
   ],
   rules: {
