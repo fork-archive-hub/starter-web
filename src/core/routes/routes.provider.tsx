c867dd510c07bb530d4235066ff087005ebe4826
Abhishek Raj <abhi@raj.me>
2020-11-01 13:15:27 +0530

    Setup Code splitting (dynamically loading code)

--- src/core/routes/routes.provider.tsx
+++ src/core/routes/routes.provider.tsx
@@ -1,27 +1,40 @@
+import React from 'react';
 import { matchPath } from 'react-router-dom';
+import baseLoadable from '@loadable/component';
+import { FallbackComponent } from 'src/utils/view';
 
 import { Route, RouteData } from 'src/core/models/route.model';
 import routes from './routes';
 
+const loadable = (func: any) => baseLoadable(func, { fallback: <FallbackComponent /> });
+
 export const routesData: RouteData[] = [
   {
     path: routes.about.path,
-    component: require('src/components/pages/about/about.component').default,
+    component: loadable(
+      () => import(/* webpackChunkName: "about", webpackPrefetch: true */ 'src/components/pages/about/about.component')
+    ),
     source: routes.about.source,
   },
   {
     path: routes.cssStylesDemo.path,
-    component: require('src/components/pages/demo/css-styles/css-styles.component').default,
+    component: loadable(
+      () => import(/* webpackChunkName: "demo", webpackPrefetch: true */ 'src/components/pages/demo/css-styles/css-styles.component') // eslint-disable-line
+    ),
     source: routes.cssStylesDemo.source,
   },
   {
     path: routes.cssInJsDemo.path,
-    component: require('src/components/pages/demo/css-in-js/css-in-js.component').default,
+    component: loadable(
+      () => import(/* webpackChunkName: "demo", webpackPrefetch: true */ 'src/components/pages/demo/css-in-js/css-in-js.component') // eslint-disable-line
+    ),
     source: routes.cssInJsDemo.source,
   },
   {
     path: routes.home.path,
-    component: require('src/components/pages/home/home.component').default,
+    component: loadable(
+      () => import(/* webpackChunkName: "home", webpackPrefetch: true */ 'src/components/pages/home/home.component')
+    ),
     source: routes.home.source,
   },
   {
---
+++ src/utils/view.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const FallbackComponent = () => <div>Loading...</div>;
