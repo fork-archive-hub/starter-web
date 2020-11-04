d2c96fb2dbfbd032126243efaa22bf5eae49c869
Abhishek Raj <abhi@raj.me>
2020-11-05 04:37:59 +0530

    Navigation for parameterized routing

--- src/components/pages/demo/fibonacci/fibonacci.component.tsx
+++ src/components/pages/demo/fibonacci/fibonacci.component.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { FibonacciData } from 'src/core/models/response.model';
 
@@ -9,6 +10,12 @@ class Fibonacci extends React.Component<FibonacciProps, FibonacciState> {
     const num = pageData?.n || '';
     const fibonacciNum = pageData?.fn || '';
 
+    const n = parseInt(num, 10);
+    const nPrev = n - 1;
+    const nNext = n + 1;
+    const npVisible = nPrev >= 0 ? 'visible' : 'hidden';
+    const nnVisible = nNext <= 20 ? 'visible' : 'hidden';
+
     return (
       <div>
         <h2>{title}</h2>
@@ -17,6 +24,18 @@ class Fibonacci extends React.Component<FibonacciProps, FibonacciState> {
         <span>
           Fibonacci Number (F<sub>{num}</sub>): {fibonacciNum}
         </span>
+        <div style={{ height: '4px' }}>&nbsp;</div>
+        <div>
+          <span style={{ visibility: npVisible }}>
+            <span>&laquo;&nbsp;</span>
+            <Link to={`./${nPrev}`}>Prev</Link>
+          </span>
+          <span style={{ display: 'inline-block', width: '25px' }}>&nbsp;</span>
+          <span style={{ visibility: nnVisible }}>
+            <Link to={`./${nNext}`}>Next</Link>
+            <span>&nbsp;&raquo;</span>
+          </span>
+        </div>
       </div>
     );
   }
