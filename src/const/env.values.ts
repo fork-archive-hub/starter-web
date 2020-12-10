9753507f09c5928a571619802974c7bcbc151674
Abhishek Raj <abhi@raj.me>
2020-12-10 19:03:02 +0530

    Optimize api request time on server [prod mode]

--- src/const/env.values.ts
+++ src/const/env.values.ts
@@ -1,4 +1,5 @@
 import { EnvValues } from 'src/core/models/common.model';
+import { checkProd, checkServer } from 'src/utils/env.utils';
 import logger from 'starter/logger';
 
 const values: EnvValues = {
@@ -6,6 +7,13 @@ const values: EnvValues = {
   portApi: process.env.PORT_API || '',
   apiBaseUrl: process.env.API_BASE_URL || '',
 };
+
+const isProd = checkProd();
+const isServer = checkServer();
+
+if (isProd && isServer) {
+  values.apiBaseUrl = `http://localhost:${values.portApi}`;
+}
 
 Object.keys(values).forEach(key => {
   if (!values[key]) {
